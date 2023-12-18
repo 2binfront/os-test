a723a7fd3cc58fcd469c2bcef04fb3fcb3f9e982
njucs <824225714@qq.com>
2023-12-18 20:33:08 +0800

    ✨ Feat: shell monitor in c

--- fork_test_3.c
+++ fork_test_3.c
@@ -6,11 +6,13 @@
 int main(){
     int val = 5;
     int pid;
-    
-    if(pid = fork())
+
+    if ((pid = fork())) {
         wait(&pid);
-	else
-		exit(val);
+
+    } else {
+      	exit(val);
+    }
     val++;
     printf("%d\n", val);
     return val;
